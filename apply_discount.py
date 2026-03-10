5d410573de9bf29cf4f82a28ea03e391b1d87e8c
Michal <michal@Michals-MacBook-Pro.local>
2026-03-10 18:14:05 +0100

    project done

--- apply_discount.py
+++ apply_discount.py
@@ -1,2 +1,15 @@
 def apply_discount(price, discount):
+
+	if not isinstance(price, (int, float)):
+		return "The price should be a number"
+	elif price <= 0:
+		return "The price should be greater than 0"
+	elif not isinstance(discount, (int, float)):
+		return "The discount should be a number"
+	elif discount < 0 or discount > 100:
+		return "The discount should be between 0 and 100"
+
+	discount_price = (price / 100) * discount
+	final_price = price - discount_price
 	
+	return final_price
